12abdd5a9673845a5016c1fdbefc50cded44f10a
nicolas-robette <nicolas.robette@free.fr>
2021-01-01 21:52:29 +0100

    fix : syntax error in ggadd_interaction

--- R/ggadd_interaction.R
+++ R/ggadd_interaction.R
@@ -9,7 +9,10 @@ ggadd_interaction <- function(p, resmca, v1, v2, sel1=1:nlevels(v1), sel2=1:nlev
   coord$cat12 <- levels(v12)
 
   coord <- coord[coord$cat1 %in% levels(v1)[sel1] & coord$cat2 %in% levels(v2)[sel2],]
-
+  coord$cat1 <- factor(coord$cat1)
+  coord$cat2 <- factor(coord$cat2)
+  coord$cat12 <- factor(coord$cat12)
+  
   p <- p + ggplot2::geom_point(data=coord, ggplot2::aes(color=.data$cat1)) +
            ggrepel::geom_text_repel(key_glyph='blank', data=coord, ggplot2::aes(color=.data$cat1, label=.data$cat12), size = textsize) +
            ggplot2::geom_line(data=coord, ggplot2::aes(color=.data$cat1)) +
