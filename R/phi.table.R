53ab2837e5b73d890ea765f27f0dbe9b68474a52
nicolas-robette <nicolas.robette@free.fr>
2021-01-10 20:36:35 +0100

    feat : phi.table() now allows weights

--- R/phi.table.R
+++ R/phi.table.R
@@ -1,10 +1,15 @@
-phi.table <- function(x,y,dec=3) {
-  tab <- cor(dichotom(factor(x),out='numeric'),
-             dichotom(factor(y),out='numeric'),
-             method='pearson',
-             use='complete.obs')
-  tab <- as.table(round(tab,dec))
+phi.table <- function(x,y,weights=rep(1,length(x)),dec=3) {
+  # tab <- cor(dichotom(factor(x),out='numeric'),
+  #            dichotom(factor(y),out='numeric'),
+  #            method='pearson',
+  #            use='complete.obs')
+  tab <- wdm::wdm(GDAtools::dichotom(factor(x),out='numeric'),
+                  GDAtools::dichotom(factor(y),out='numeric'),
+                  method = "pearson",
+                  weights = weights,
+                  remove_missing = TRUE)
+  tab <- as.table(tab)
   rownames(tab) <- levels(factor(x))
   colnames(tab) <- levels(factor(y))
-  tab
+  return(round(tab,dec))
 }
