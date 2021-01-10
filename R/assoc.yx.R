9b2180034eec9861d41eb9bef57fc8f6a24a1ac8
nicolas-robette <nicolas.robette@free.fr>
2021-01-10 22:22:55 +0100

    feat : assoc.yx() now allows weights

--- R/assoc.yx.R
+++ R/assoc.yx.R
@@ -1,4 +1,4 @@
-assoc.yx <- function(y,x,xx=TRUE,twocont="kendall",nperm=1000,distrib="asympt",dec.a=3,dec.p=3) {
+assoc.yx <- function(y,x,weights=rep(1,length(y)),xx=TRUE,twocont="kendall",nperm=1000,distrib="asympt",dec.a=3,dec.p=3) {
   
   x <- as.data.frame(x)
   xnames <- names(x)
@@ -9,25 +9,25 @@ assoc.yx <- function(y,x,xx=TRUE,twocont="kendall",nperm=1000,distrib="asympt",d
   for(i in 1:ncol(x)) {
     # print(i)
     if(yformat=='numeric' & xformats[i] %in% c('numeric','integer')) {
-      z <- GDAtools::assoc.twocont(y, x[,i], nperm=nperm, distrib=distrib)
+      z <- GDAtools::assoc.twocont(y, x[,i], weights=weights, nperm=nperm, distrib=distrib)
       measure = twocont
       association = z[,twocont][1]
       permutation.pvalue = z[,twocont][2]
     }
     if(yformat=='numeric' & xformats[i]=='factor') {
-      z <- GDAtools::assoc.catcont(x[,i], y, nperm=nperm, distrib=distrib)
+      z <- GDAtools::assoc.catcont(x[,i], y, weights=weights, nperm=nperm, distrib=distrib)
       measure='Eta2'
       association = z$eta.squared
       permutation.pvalue = z$permutation.pvalue
     }
     if(yformat=='factor' & xformats[i]%in% c('numeric','integer')) {
-      z <- GDAtools::assoc.catcont(y, x[,i], nperm=nperm, distrib=distrib)
+      z <- GDAtools::assoc.catcont(y, x[,i], weights=weights, nperm=nperm, distrib=distrib)
       measure='Eta2'
       association = z$eta.squared
       permutation.pvalue = z$permutation.pvalue
     }
     if(yformat=='factor' & xformats[i]=='factor') {
-      z <- GDAtools::assoc.twocat(x[,i], y, nperm=nperm, distrib=distrib)
+      z <- GDAtools::assoc.twocat(x[,i], y, weights=weights, nperm=nperm, distrib=distrib)
       measure="Cramer's V"
       association = z$cramer.v
       permutation.pvalue = z$permutation.pvalue
@@ -52,25 +52,25 @@ assoc.yx <- function(y,x,xx=TRUE,twocont="kendall",nperm=1000,distrib="asympt",d
       x2 <- x[,combi[[i]][2]]
       
       if(class(x1) %in% c('numeric','integer') & class(x2) %in% c('numeric','integer')) {
-        z <- GDAtools::assoc.twocont(x1, x2, nperm=nperm, distrib=distrib)
+        z <- GDAtools::assoc.twocont(x1, x2, weights=weights, nperm=nperm, distrib=distrib)
         measure = twocont
         association = z[,twocont][1]
         permutation.pvalue = z[,twocont][2]
       }
       if(class(x1) %in% c('numeric','integer') & class(x2)=='factor') {
-        z <- GDAtools::assoc.catcont(x2, x1, nperm=nperm, distrib=distrib)
+        z <- GDAtools::assoc.catcont(x2, x1, weights=weights, nperm=nperm, distrib=distrib)
         measure='Eta2'
         association = z$eta.squared
         permutation.pvalue = z$permutation.pvalue
       }
       if(class(x1)=='factor' & class(x2) %in% c('numeric','integer')) {
-        z <- GDAtools::assoc.catcont(x1, x2, nperm=nperm, distrib=distrib)
+        z <- GDAtools::assoc.catcont(x1, x2, weights=weights, nperm=nperm, distrib=distrib)
         measure='Eta2'
         association = z$eta.squared
         permutation.pvalue = z$permutation.pvalue
       }
       if(class(x1)=='factor' & class(x2)=='factor') {
-        z <- GDAtools::assoc.twocat(x1, x2, nperm=nperm, distrib=distrib)
+        z <- GDAtools::assoc.twocat(x1, x2, weights=weights, nperm=nperm, distrib=distrib)
         measure="Cramer's V"
         association = z$cramer.v
         permutation.pvalue = z$permutation.pvalue
