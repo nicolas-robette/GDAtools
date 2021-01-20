58731ffa98c898fc37564b4e681bc9809f1fe3b7
nicolas-robette <nicolas.robette@free.fr>
2021-01-20 22:01:10 +0100

    permutation p-values in assoc.catcont()

--- R/assoc.catcont.R
+++ R/assoc.catcont.R
@@ -1,6 +1,6 @@
 assoc.catcont <- function(x,y,weights=rep(1,length(x)),nperm=1000,distrib="asympt",digits=3) {
 
-    eta.squared <- summary.lm(aov(y~x,weights=weights))$r.squared
+  eta.squared <- summary.lm(aov(y~x,weights=weights))$r.squared
   
   if(!is.null(nperm)) {
     h0distrib <- numeric()
@@ -19,6 +19,27 @@ assoc.catcont <- function(x,y,weights=rep(1,length(x)),nperm=1000,distrib="asymp
   for(i in 1:nlevels(x)) cor.coeff[i] <- wdm::wdm(as.numeric(x==levels(x)[i]), y, 
                                                   method="pearson", weights=weights, remove_missing=TRUE)
   names(cor.coeff) <- levels(x)
+  
+  if(!is.null(nperm)) {
+    ppval <- numeric(length=nlevels(x))
+    for(i in 1:nlevels(x)) {
+      obs <- cor.coeff[i]
+      h0d <- numeric()
+      for(j in 1:nperm) h0d[j] <- wdm::wdm(as.numeric(x==levels(x)[i]), sample(y), 
+                                           method="pearson", weights=weights, remove_missing=TRUE)
+      if(distrib=='approx') {
+        if(obs>=0) ppval[i] <- sum(obs<=h0d)/nperm
+        if(obs<0) ppval[i] <- sum(obs>h0d)/nperm
+      } else {
+        fit <- MASS::fitdistr(h0d,"normal")$estimate
+        if(obs>=0) ppval[i] <- 1-stats::pnorm(obs,fit[1],fit[2])
+        if(obs<0) ppval[i] <- stats::pnorm(obs,fit[1],fit[2])
+      }
+    }
+    names(ppval) <- levels(x)
+  }
+  if(is.null(nperm)) ppval <- NULL
+
   cor.coeff <- round(cor.coeff,digits)
-  return(list('eta.squared'=eta.squared, 'permutation.pvalue'=permutation.pvalue, 'cor.coeff'=cor.coeff))
+  return(list('eta.squared'=eta.squared, 'permutation.pvalue'=permutation.pvalue, 'cor.coeff'=cor.coeff, 'cor.perm.pval'=ppval))
 }
