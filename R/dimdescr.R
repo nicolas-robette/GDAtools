dcafbda782b542bf8c36a8acc83efacca9af9688
nicolas-robette <nicolas.robette@free.fr>
2021-04-11 21:53:30 +0200

    dimdescr() replaces dimdesc.MCA()

---
+++ R/dimdescr.R
@@ -0,0 +1,14 @@
+dimdescr <- function(resmca,ncp=1:3,min.cor=NULL,nperm=100,distrib="asympt") {
+   res <- list()
+   X <- resmca$call$X
+   classe <- class(resmca)[1]
+   if(classe=='stMCA') classe=resmca$call$input.mca
+   for(i in ncp) {
+      if(classe %in% c('MCA','speMCA')) temp <- condesc(resmca$ind$coord[,i],X,min.cor=min.cor,nperm=nperm,distrib=distrib)
+      if(classe == 'csMCA') temp <- condesc(resmca$ind$coord[,i],X[resmca$call$subcloud,],min.cor=min.cor,nperm=nperm,distrib=distrib)
+      rownames(temp) <- NULL
+      res[[i]] <- temp
+      }
+   names(res) <- paste('dim',ncp,sep='.')
+   return(res)
+}
