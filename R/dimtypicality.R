e6c2db91e923126f713ebee53cd750b03f879614
nicolas-robette <nicolas.robette@free.fr>
2021-04-14 23:38:53 +0200

    new dimtypicality() function

---
+++ R/dimtypicality.R
@@ -0,0 +1,24 @@
+dimtypicality <- function(resmca,vars,dim=1:resmca$call$ncp,max.pval=1) {
+  n=names(vars)
+  res1 <- list()
+  res2 <- list()
+  for(i in 1:length(vars)) {
+    temp <- varsup(resmca,vars[[i]])
+    res1[[i]] <- temp$typic
+    rownames(res1[[i]]) <- paste(n[i],rownames(res1[[i]]),sep='.')
+    res2[[i]] <- temp$weight
+    }
+  res1 <- do.call('rbind.data.frame',res1)
+  res2 <- unlist(res2)
+  res <- list()
+  for(i in 1:length(dim)) {
+    z <- data.frame(weight=res2,test.stat=res1[,i])
+    z$p.value <- 2*(1 -pnorm(abs(z$test.stat)))
+    rownames(z) <- rownames(res1)
+    z <- z[order(-z$test.stat),]
+    z <- z[z$p.value<=max.pval,]
+    res[[i]] <- z
+    }
+  names(res) <- paste('dim',dim,sep='.')
+  return(res)
+}
