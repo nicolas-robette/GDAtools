dde38f3a606dc572a8503f27036a95eb7199604c
nicolas-robette <nicolas.robette@free.fr>
2021-04-14 23:40:52 +0200

    homog.test() computes p-values

--- R/homog.test.R
+++ R/homog.test.R
@@ -3,9 +3,13 @@ homog.test <- function(resmca,var) {
   N <- sum(vs$weight)
   a <- sqrt(1/outer(1/vs$weight,1/vs$weight,"+"))
   ncp <- resmca$call$ncp
-  dev <- list()
-  for(i in 1:ncp) dev[[i]] <- abs(outer(vs$coord[,i],vs$coord[,i],"-"))/sqrt(resmca$eig[[1]][i])
-  res <- lapply(dev,function(x) a*sqrt((N-1)/N)*x)
+  res <- vector("list",ncp)
+  for(i in 1:ncp) {
+    temp <- abs(outer(vs$coord[,i],vs$coord[,i],"-"))/sqrt(resmca$eig[[1]][i])
+    test.stat <- a*sqrt((N-1)/N)*temp
+    res[[i]]$test.stat <- test.stat
+    res[[i]]$p.values <- 2*(1 -pnorm(test.stat))
+  }
   names(res) <- paste('dim',1:ncp,sep='.')
   return(res)
-  }
+}
