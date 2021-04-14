e7b8a870dca1df41b41a8bf0756452cf6f961f0f
nicolas-robette <nicolas.robette@free.fr>
2021-04-14 23:43:43 +0200

    argument l renamed to vars in dimeta2()

--- R/dimeta2.R
+++ R/dimeta2.R
@@ -1,12 +1,13 @@
-dimeta2 <- function(resmca,l,n=names(l),dim=1:resmca$call$ncp) {
-  eta2 <- matrix(nrow=length(l),ncol=length(dim))
+dimeta2 <- function(resmca,vars,dim=1:resmca$call$ncp) {
+  n=names(vars)
+  eta2 <- matrix(nrow=ncol(vars),ncol=length(dim))
   sub <- rep(TRUE,times=nrow(resmca$ind$coord))
   if(class(resmca)[1]=='csMCA') sub <- resmca$call$subcloud
-  if(class(resmca)[1]=='stMCA') if(resmca$call$input.mca=='csMCA') sub <- resmca$call$subcloud #new
-  ww <- resmca$call$row.w[sub] #new
-  if(class(resmca)[1]=='stMCA') ww <- resmca$call$fit$weights #new
+  if(class(resmca)[1]=='stMCA') if(resmca$call$input.mca=='csMCA') sub <- resmca$call$subcloud
+  ww <- resmca$call$row.w[sub]
+  if(class(resmca)[1]=='stMCA') ww <- resmca$call$fit$weights
   for(i in 1:length(dim)) {
-    for(j in 1:length(l)) eta2[j,dim[i]] <- summary(lm(resmca$ind$coord[,dim[i]]~l[[j]][sub],weights=ww))$r.squared
+    for(j in 1:ncol(vars)) eta2[j,dim[i]] <- summary(lm(resmca$ind$coord[,dim[i]]~vars[,j][sub],weights=ww))$r.squared
    }
   rownames(eta2) <- n
   colnames(eta2) <- colnames(resmca$var$contrib)[dim]
