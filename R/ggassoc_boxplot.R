22c31beff609d345ce46e40fc5dded62d2863979
nicolas-robette <nicolas.robette@free.fr>
2021-04-23 23:04:41 +0200

    new options in ggassoc_boxplot()

--- R/ggassoc_boxplot.R
+++ R/ggassoc_boxplot.R
@@ -1,4 +1,4 @@
-ggassoc_boxplot <- function(data, mapping, axes.labs=TRUE, ticks.labs=TRUE, text.size=3) {
+ggassoc_boxplot <- function(data, mapping, axes.labs=TRUE, ticks.labs=TRUE, text.size=3, box=TRUE, notch=FALSE, violin=TRUE) {
   xVal <- GGally::eval_data_col(data, mapping$x)
   yVal <- GGally::eval_data_col(data, mapping$y)
   xName <- rlang::as_name(mapping$x)
@@ -10,9 +10,13 @@ ggassoc_boxplot <- function(data, mapping, axes.labs=TRUE, ticks.labs=TRUE, text
   }
   newdata <- data.frame(xVal,yVal)
   if(is.factor(newdata$yVal)) newdata$yVal <- factor(newdata$yVal, levels=rev(levels(newdata$yVal)))
-  p <- ggplot2::ggplot(newdata, ggplot2::aes(x=.data$xVal, y=.data$yVal)) +
-          ggplot2::geom_violin(scale="count", alpha=.6, color="grey") +
-          ggplot2::geom_boxplot(varwidth=TRUE, notch=TRUE, fill="grey", alpha=.6, outlier.size=0.5, outlier.alpha=0.2) +
+  p <- ggplot2::ggplot(newdata, ggplot2::aes(x=.data$xVal, y=.data$yVal))
+  
+  if(violin) p <- p + ggplot2::geom_violin(scale="count", alpha=.6, color="grey")
+  
+  if(box) p <- p + ggplot2::geom_boxplot(varwidth=TRUE, notch=notch, fill="grey", alpha=.6, outlier.size=0.5, outlier.alpha=0.2)
+  
+  p <- p + 
           ggplot2::theme_minimal() +
           ggplot2::theme( legend.position="none",
                           panel.grid.major = ggplot2::element_blank(),
