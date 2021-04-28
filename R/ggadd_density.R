ebad42ab29c825eaa2b090b4b6f85cfe18553881
nicolas-robette <nicolas.robette@free.fr>
2021-04-28 22:39:49 +0200

    fix for ellipses in ggadd_density() and website update

--- R/ggadd_density.R
+++ R/ggadd_density.R
@@ -1,10 +1,3 @@
-# p <- ggcloud_indiv(ACM, col="lightgray")
-# ggadd_density(p, ACM, CRIT$Ctelerama, "5")
-# ggadd_density(p, ACM, CRIT$Ctelerama, "5", ellipse=TRUE, col.ellipse="darkgreen")
-# ggadd_density(p, ACM, CRIT$Ctelerama, "5", density="area")
-# ggadd_density(p, ACM, CRIT$Ctelerama, "5", density="area", pal.area="magma", alpha.area=0.1)
-# ggadd_density(p, ACM, CRIT$Ctelerama, "5", density="area", ellipse=TRUE)
-
 ggadd_density <- function(p, resmca, var, cat=levels(var)[1], axes=c(1,2),
                           density="contour", col.contour="darkred", pal.area="viridis", alpha.area=0.2,
                           ellipse=FALSE, col.ellipse="black"){
@@ -19,7 +12,8 @@ ggadd_density <- function(p, resmca, var, cat=levels(var)[1], axes=c(1,2),
                                ggplot2::scale_fill_continuous(type="viridis", option=pal.area) +
                                ggplot2::theme(legend.position="none")
     
-  if(ellipse==TRUE) p <- p + ggplot2::stat_ellipse(data=df, ggplot2::aes(x=.data$dim.1, y=.data$dim.2), colour=col.ellipse, level=0.86, type='norm', size=.2)
-    
+  #if(ellipse==TRUE) p <- p + ggplot2::stat_ellipse(data=df, ggplot2::aes(x=.data$dim.1, y=.data$dim.2), colour=col.ellipse, level=0.86, type='norm', size=.2)
+  if(ellipse==TRUE) p <- ggadd_kellipses(p=p, resmca=resmca, var=var, sel=which(levels(var)==cat), axes=axes, col=col.ellipse, size=0.3, label=FALSE, points=FALSE, legend="none")
+  
   return(p)
 }
