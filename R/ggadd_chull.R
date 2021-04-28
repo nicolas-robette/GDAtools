7a4b2da56cf4b430de99eb4d4481d933d2a46424
nicolas-robette <nicolas.robette@free.fr>
2021-04-28 22:01:42 +0200

    new function : ggadd_chull()

---
+++ R/ggadd_chull.R
@@ -0,0 +1,42 @@
+ggadd_chull <- function(p, resmca, var, sel=1:nlevels(var), axes=c(1,2), col=NULL, alpha=0.2, label=TRUE, label.size=5, legend="right") {
+
+  ecoord <- as.data.frame(resmca$ind$coord[,axes])
+  names(ecoord) <- c('axeX','axeY')
+  ecoord$var <- var
+  ecoord <- ecoord[var %in% levels(var)[sel],]
+  ecoord$var <- factor(ecoord$var)
+  
+  vs <- varsup(resmca,var)
+  ccoord <- as.data.frame(vs$coord[,axes])
+  names(ccoord) <- c('axeX','axeY')
+  ccoord$categories <- names(vs$weight)
+  ccoord <- ccoord[sel,]
+  ccoord$axeX <- ccoord$axeX*resmca$svd$vs[axes[1]]
+  ccoord$axeY <- ccoord$axeY*resmca$svd$vs[axes[2]]
+  
+  pfin <- p + stat_chull(data=ecoord, ggplot2::aes(x=.data$axeX, y=.data$axeY, colour=.data$var, fill=.data$var), alpha=alpha)
+  
+  if(label) pfin <- pfin + ggplot2::geom_text(key_glyph='blank', data=ccoord, ggplot2::aes(x=.data$axeX, y=.data$axeY, label=.data$categories, colour=.data$categories), size=label.size)
+
+  if(!is.null(col)) {
+    if(length(col)>1) { pfin <- pfin + ggplot2::scale_colour_manual(values = col) +
+                                       ggplot2::scale_fill_manual(values = col)
+    } else if(length(col)==1) {
+      if(col %in% rownames(RColorBrewer::brewer.pal.info)) { pfin <- pfin + ggplot2::scale_color_brewer(palette = col) +
+                                                                            ggplot2::scale_fill_brewer(palette = col)
+      } else if(col=='bw') { pfin <- pfin + ggplot2::scale_color_grey() +
+                                            ggplot2::scale_fill_grey()
+      } else if(is.character(col)) { pfin <- pfin + ggplot2::scale_colour_manual(values = rep(col,nrow(ecoord))) +
+                                                    ggplot2::scale_fill_manual(values = rep(col,nrow(ecoord)))
+        }
+    }
+  }
+    
+  pfin <- pfin + ggplot2::guides(color = ggplot2::guide_legend(title=""),
+                                 fill =  ggplot2::guide_legend(title="")) +
+                 ggplot2::theme(legend.position = legend)
+  
+  pfin
+}
+
+
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,21 @@
+StatChull <- ggproto("StatChull", Stat,
+                     compute_group = function(data, scales) {
+                       data[chull(data$x, data$y), , drop = FALSE]
+                     },
+                     
+                     required_aes = c("x", "y")
+)
+
+stat_chull <- function(mapping = NULL, data = NULL, geom = "polygon",
+                       position = "identity", na.rm = FALSE, show.legend = NA, 
+                       inherit.aes = TRUE, ...) {
+  layer(
+    stat = StatChull, data = data, mapping = mapping, geom = geom, 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
 weighted.quantile <- function(x, w, probs = .5, method = "raw") {
   if(method=="raw") {
     w <- w[order(x)]
