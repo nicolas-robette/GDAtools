9afdbed74ce42aa30e8381f6152acbfa75a271f7
nicolas-robette <nicolas.robette@free.fr>
2021-05-06 23:40:42 +0200

    compatibility of homog.test() with various types of MCAs

--- R/homog.test.R
+++ R/homog.test.R
@@ -1,15 +1,19 @@
-homog.test <- function(resmca,var) {
+homog.test <- function(resmca,var,dim=c(1,2)) {
+
+    type <- attr(resmca,'class')[1]
+  if(type %in% c("MCA","stMCA","multiMCA")) eigen <- resmca$eig[,"eigenvalue"]
+  if(type %in% c("speMCA","csMCA")) eigen <- resmca$eig$eigen
+
   vs <- varsup(resmca,var)
   N <- sum(vs$weight)
   a <- sqrt(1/outer(1/vs$weight,1/vs$weight,"+"))
-  ncp <- resmca$call$ncp
-  res <- vector("list",ncp)
-  for(i in 1:ncp) {
-    temp <- abs(outer(vs$coord[,i],vs$coord[,i],"-"))/sqrt(resmca$eig[[1]][i])
+  res <- vector("list",length(dim))
+  for(i in 1:length(dim)) {
+    temp <- abs(outer(vs$coord[,dim[i]],vs$coord[,dim[i]],"-"))/sqrt(eigen[dim[i]])
     test.stat <- a*sqrt((N-1)/N)*temp
     res[[i]]$test.stat <- test.stat
     res[[i]]$p.values <- 2*(1 -pnorm(test.stat))
   }
-  names(res) <- paste('dim',1:ncp,sep='.')
+  names(res) <- paste('dim',dim,sep='.')
   return(res)
 }
