135647e506e362983c6aeed48c9eba081e02caf5
nicolas-robette <nicolas.robette@free.fr>
2021-05-18 14:40:11 +0200

    new ggadd_supind() function

---
+++ R/ggadd_supind.R
@@ -0,0 +1,15 @@
+ggadd_supind <- function(p, resmca, dfsup, axes=c(1,2), col="black", textsize=5, pointsize=2) {
+  dt <- as.data.frame(indsup(resmca, dfsup)$coord[,axes])
+  names(dt) <- c('axeX','axeY')
+  dt$names <- rownames(dt)
+  if(is.null(pointsize)) {
+    pfin <- p +
+              ggrepel::geom_text_repel(data=dt, ggplot2::aes(x=.data$axeX, y=.data$axeY, label=.data$names), col=col, size=textsize)
+  }
+  if(!is.null(pointsize)) {
+    pfin <- p +
+              ggplot2::geom_point(data=dt, ggplot2::aes(x=.data$axeX, y=.data$axeY), col=col, size=pointsize) +
+              ggrepel::geom_text_repel(data=dt, ggplot2::aes(x=.data$axeX, y=.data$axeY, label=.data$names), col=col, size=textsize)
+  }
+  pfin
+}
