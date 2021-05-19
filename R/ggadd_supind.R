29f10550eb84879781a7da65a319970d5f47aa80
nicolas-robette <nicolas.robette@free.fr>
2021-05-19 10:43:32 +0200

    fix repel overlap

--- R/ggadd_supind.R
+++ R/ggadd_supind.R
@@ -4,12 +4,12 @@ ggadd_supind <- function(p, resmca, dfsup, axes=c(1,2), col="black", textsize=5,
   dt$names <- rownames(dt)
   if(is.null(pointsize)) {
     pfin <- p +
-              ggrepel::geom_text_repel(data=dt, ggplot2::aes(x=.data$axeX, y=.data$axeY, label=.data$names), col=col, size=textsize)
+              ggrepel::geom_text_repel(data=dt, ggplot2::aes(x=.data$axeX, y=.data$axeY, label=.data$names), col=col, size=textsize, max.overlaps=Inf)
   }
   if(!is.null(pointsize)) {
     pfin <- p +
               ggplot2::geom_point(data=dt, ggplot2::aes(x=.data$axeX, y=.data$axeY), col=col, size=pointsize) +
-              ggrepel::geom_text_repel(data=dt, ggplot2::aes(x=.data$axeX, y=.data$axeY, label=.data$names), col=col, size=textsize)
+              ggrepel::geom_text_repel(data=dt, ggplot2::aes(x=.data$axeX, y=.data$axeY, label=.data$names), col=col, size=textsize, max.overlaps=Inf)
   }
   pfin
 }
