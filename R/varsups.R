a14cd03b5846da4166d8ba56d89718a88d061817
Nicolas <nicolas.robette@free.fr>
2022-12-24 11:39:17 +0100

    varsups function

---
+++ R/varsups.R
@@ -0,0 +1,23 @@
+# library(GDAtools)
+# data(Music)
+# getindexcat(Music)
+# resmca <- speMCA(Music[,1:5],excl=c(3,6,9,12,15))
+# vars <- Music[,6:9]
+# varsups(resmca, vars)
+
+varsups <- function(resmca, vars) {
+  if(any(sapply(vars, FUN = function(x) !is.factor(x)))) stop("variables in data should all be factors")
+  # nlev <- sapply(vars, nlevels)
+  # vnames <- unlist(tapply(nlev, nlev, FUN = function(x) rep(names(x), x)))
+  tmp <- lapply(vars, function(x) varsup(resmca, x))
+  res <- list()
+  res$weight <- unlist(lapply(tmp, function(x) x$weight))
+  res$coord <- do.call("rbind.data.frame", lapply(tmp, function(x) x$coord))
+  res$cos2 <- do.call("rbind.data.frame", lapply(tmp, function(x) x$cos2))
+  res$var <- lapply(tmp, function(x) x$var)
+  res$typic <- do.call("rbind.data.frame", lapply(tmp, function(x) x$typic))
+  res$pval <- do.call("rbind.data.frame", lapply(tmp, function(x) x$pval))
+  res$cor <- do.call("rbind.data.frame", lapply(tmp, function(x) x$cor))
+  return(res)
+}
+
