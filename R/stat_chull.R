073a41c12c70e06678699ec99b26960356b3f01d
Nicolas <nicolas.robette@free.fr>
2023-02-27 13:00:35 +0100

    website update

--- R/stat_chull.R
+++ R/stat_chull.R
@@ -1,4 +1,5 @@
-StatChull <- ggproto("StatChull", Stat,
+StatChull <- ggproto("StatChull",
+                     Stat,
                      compute_group = function(data, scales) {
                        data[chull(data$x, data$y), , drop = FALSE]
                      },
@@ -6,12 +7,21 @@ StatChull <- ggproto("StatChull", Stat,
                      required_aes = c("x", "y")
 )
 
-stat_chull <- function(mapping = NULL, data = NULL, geom = "polygon",
-                       position = "identity", na.rm = FALSE, show.legend = NA, 
+stat_chull <- function(mapping = NULL,
+                       data = NULL,
+                       geom = "polygon",
+                       position = "identity",
+                       na.rm = FALSE,
+                       show.legend = NA, 
                        inherit.aes = TRUE, ...) {
   layer(
-    stat = StatChull, data = data, mapping = mapping, geom = geom, 
-    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    stat = StatChull,
+    data = data,
+    mapping = mapping,
+    geom = geom, 
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
     params = list(na.rm = na.rm, ...)
   )
 }
