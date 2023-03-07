fd99145cde8a77be92e1c44865660481799ff43f
Nicolas <nicolas.robette@free.fr>
2023-03-07 22:49:57 +0100

    changes in tabcontrib()

--- R/tabcontrib.R
+++ R/tabcontrib.R
@@ -1,21 +1,95 @@
-tabcontrib <- function(resmca, dim=1, best=TRUE) {
-	z <- dimcontrib(resmca,dim,best=best)$var[[1]]
-	z$var <- unlist(lapply(strsplit(rownames(z),'.',fixed=TRUE),function(x) x[1]))
-	z$moda <- unlist(lapply(strsplit(rownames(z),'.',fixed=TRUE),function(x) paste(x[-1],collapse='.')))
-	w <- aggregate(abs(z$ctr),by=list(z$var),sum)
-	w <- w[order(w$x,decreasing=TRUE),]
-	names(w) <- c('var','ctrtot')
-	w$rank <- 1:nrow(w)
-	wz <- merge(z,w,by='var',all.x=TRUE,all.y=FALSE)
-	wz <- wz[order(wz$rank,wz$ctr),]
-	wz$count <- unlist(lapply(rle(as.vector(wz$var))$lengths,function(x){1:x}))
-	wz$ctr2 <- wz$ctr1 <- wz$ctr
-	wz$ctr1[wz$ctr1>0] <- ""
-	wz$ctr2[wz$ctr2<0] <- ""
-	wz$var[wz$count>1] <- ""
-	wz$ctrtot[wz$count>1] <- ""
-	wz$cumctrtot <- character(length=nrow(wz))
-	wz$cumctrtot[!wz$ctrtot==""] <- cumsum(as.numeric(wz$ctrtot[!wz$ctrtot==""]))
-	wz <- wz[,c('var','moda','ctr1','ctr2','weight','ctrtot','cumctrtot')]
-	return(wz)
-	}
+# data(Taste)
+# resmca <- speMCA(Taste[,1:11], excl=c(3,6,9,12,15,18,21,24,27,30,33))
+# 
+# data(Music)
+# resmca <- speMCA(Music[,1:5],excl=c(3,6,9,12,15))
+# 
+# df <- readxl::read_xls("/home/nicolas/Nextcloud/R/My_packages/GDAtools/devel/Taste_Example.xls")[,-1]
+# df <- as.data.frame(lapply(df, factor))
+# resmca <- speMCA(df[df$Isup=="Active", 2:5])
+# 
+# dim = 1
+# best = TRUE
+# dec = 2
+# 
+# tabcontrib(resmca, dim = 3, best = TRUE)
+
+tabcontrib <- function(resmca, dim = 1, best = TRUE, dec = 2) {
+  
+  getvarnames <- function(resmca) {
+    X <- resmca$call$X
+    var <- unlist(lapply(names(X), function(x) rep(x, nlevels(X[[x]]))))
+    cat <- unlist(lapply(X, levels))
+    varcat <- paste(var, cat, sep = ".")
+    res <- data.frame(var, cat, varcat)
+    rownames(res) <- NULL
+    return(res)
+  }
+  
+  # initial data frame of contributions
+  df1 <- data.frame(varcat = names(resmca$var$weight), weight = resmca$var$weight)
+  df2 <- data.frame(varcat = rownames(resmca$var$coord), coord = resmca$var$coord[,dim])
+  df3 <- data.frame(varcat = rownames(resmca$var$contrib), ctr = resmca$var$contrib[,dim])
+  df <- merge(merge(merge(getvarnames(resmca), df1, by = "varcat"), df2, by = "varcat"), df3, by = "varcat")
+  df$sign <- sign(df$coord)
+  
+  if(best) df <- df[df$ctr >= 100/nrow(df),]
+  
+  # ctr by variable
+  w <- aggregate(cbind(ctrtot = ctr) ~ var, data = df, FUN = sum)
+  # w <- w[order(w$ctrtot, decreasing = TRUE),]
+  w$rank <- rank(-w$ctrtot)
+  w <- w[order(w$rank),]
+  w$cumctr <- cumsum(w$ctrtot)
+  
+  # barycentres
+  temp <- df
+  temp$wsum <- temp$weight * temp$coord
+  b1 <- aggregate(wsum ~ sign + var, data = temp, FUN = sum)
+  b2 <- aggregate(cbind(totw = weight) ~ sign + var, data = temp, FUN = sum)
+  bary <- merge(b1, b2, by = c("var", "sign"))
+  bary$coord <- bary$wsum / bary$totw
+  bary$wsum <- NULL
+  
+  # ctr of deviation
+  d1 <- aggregate(cbind(nd = 1/totw) ~ var, data = bary, FUN = sum)
+  d1$nd <- 1/d1$nd
+  d2 <- aggregate(cbind(dev = abs(coord)) ~ var, data = bary, FUN = sum)
+  devi <- merge(d1, d2, by = "var")
+  n <- nrow(resmca$call$X)
+  Q <- ncol(resmca$call$X)
+  eig <- resmca$eig$eig[dim]
+  devi$ctrdev <- devi$nd * devi$dev * devi$dev / (Q * n * eig)
+  devi <- devi[, c("var", "ctrdev")]
+  
+  # proportion to question
+  prop <- data.frame(var = rownames(resmca$var$v.contrib), ctrvar = resmca$var$v.contrib[,dim])
+  prop <- merge(prop, devi, by = "var")
+  prop$ctrvar <- 100 * prop$ctrdev / prop$ctrvar
+  prop$ctrdev <- NULL
+  
+  # all together
+  res <- merge(df, w, by = "var", all.x = TRUE, all.y = FALSE)
+  res <- merge(res, devi, by = "var", all.x = TRUE, all.y = FALSE)
+  res <- merge(res, prop, by = "var", all.x = TRUE, all.y = FALSE)
+  res <- res[order(res$rank, -res$ctr),]
+  res$count <- unlist(lapply(rle(as.vector(res$var))$lengths, function(x) {1:x}))
+  res$ctr2 <- res$ctr1 <- res$ctr
+  res$ctr1 <- round(res$ctr1, dec)
+  res$ctr2 <- round(res$ctr2, dec)
+  res$ctrtot <- round(res$ctrtot, dec)
+  res$cumctr <- round(res$cumctr, dec)
+  res$ctrdev <- round(100*res$ctrdev, dec)
+  res$ctrvar <- round(100*res$ctrvar, dec)
+  res$ctr1[res$sign==1] <- ""
+  res$ctr2[res$sign==-1] <- ""
+  res$var[res$count>1] <- ""
+  res$ctrtot[res$count>1] <- ""
+  res$cumctr[res$count>1] <- ""
+  res$ctrdev[res$count>1] <- ""
+  res$ctrvar[res$count>1] <- ""
+  res <- res[, c("var", "cat", "weight", "ctr1", "ctr2", "ctrtot", "cumctr", "ctrdev", "ctrvar")]
+  names(res) <- c("Variable", "Category", "Weight", "Contribution (left)", "Contribution (right)",
+                  "Total contribution", "Cumulated contribution", "Contribution of deviation", "Proportion to variable")
+  return(res)
+}
