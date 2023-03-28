9e04fe532249f1f450586d6c2d52c25ff2b3652d
Nicolas <nicolas.robette@free.fr>
2023-03-28 22:26:16 +0200

    new excl argument for bcMCA

--- R/bcMCA.R
+++ R/bcMCA.R
@@ -1,23 +1,30 @@
-bcMCA <- function(data, class, row.w = NULL, ncp = 5) {
+bcMCA <- function(data, class, excl = NULL, row.w = NULL, ncp = 5) {
+  
   if(is.null(row.w)) row.w <- rep(1, nrow(data))
   if(any(sapply(data, FUN = function(x) !is.factor(x)))) stop("variables in data should all be factors")
   if(!is.factor(class)) stop("class should be a factor")
+  if(is.character(excl)) excl <- which(getindexcat(data) %in% excl)
+  if(is.null(excl)) excl <- 99999
+  
   n <- sum(row.w)
   nk <- tapply(row.w, class, sum)
   pk <- as.vector(nk / n)
-  disj <- dichotom(data, out = "numeric")
+  disj <- dichotom(data, out = "numeric")[,-excl]
   centers <- sweep(t(as.matrix(dichotom(class)))%*%diag(row.w)%*%as.matrix(disj), 1, nk, "/")
   centers <- data.frame(centers)
   row.names(centers) <- levels(class)
   names(centers) <- names(disj)
   df <- rbind.data.frame(centers, disj)
+  
   res <- FactoMineR::CA(df, 
                         row.w = as.vector(pk), 
                         row.sup = (nlevels(class)+1):nrow(df), 
                         ncp = ncp, 
                         graph = FALSE)
+  
   mca <- FactoMineR::CA(disj, row.w = row.w, ncp = ncp, graph = FALSE)
   res$ratio <- sum(res$eig[,"eigenvalue"]) / sum(mca$eig[,"eigenvalue"])
+  
   class(res) <- c("CA", "bcMCA", "list")
   return(res)
 }
