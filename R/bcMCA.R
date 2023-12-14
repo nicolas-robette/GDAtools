f3fa7c36f8860a1f47c1cfbeb199eb2a9ba24b9a
Nicolas <nicolas.robette@free.fr>
2023-12-14 10:50:40 +0100

    fixed proble with excl argument

--- R/bcMCA.R
+++ R/bcMCA.R
@@ -4,12 +4,12 @@ bcMCA <- function(data, class, excl = NULL, row.w = NULL, ncp = 5) {
   if(any(sapply(data, FUN = function(x) !is.factor(x)))) stop("variables in data should all be factors")
   if(!is.factor(class)) stop("class should be a factor")
   if(is.character(excl)) excl <- which(getindexcat(data) %in% excl)
-  if(is.null(excl)) excl <- 99999
-  
+
   n <- sum(row.w)
   nk <- tapply(row.w, class, sum)
   pk <- as.vector(nk / n)
-  disj <- dichotom(data, out = "numeric")[,-excl]
+  disj <- dichotom(data, out = "numeric")
+  if(!is.null(excl)) disj <- disj[,-excl]
   centers <- sweep(t(as.matrix(dichotom(class)))%*%diag(row.w)%*%as.matrix(disj), 1, nk, "/")
   centers <- data.frame(centers)
   row.names(centers) <- levels(class)
